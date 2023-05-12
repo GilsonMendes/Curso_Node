fc07d146f9c44d449053360d851481a6bee327c5
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-05-12 13:15:23 -0300

    Add file system aula

---
+++ modulo_http/src/module_fs.ts
@@ -0,0 +1,18 @@
+import { ServerResponse } from "http";
+import chalk from 'chalk';
+
+const http = require("http");
+const fs = require("fs");
+
+const port = 3000;
+const server = http.createServer((req: Response, res: ServerResponse) => {
+  fs.readFile("arquivoFS.html", function (err: any, data: any) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
