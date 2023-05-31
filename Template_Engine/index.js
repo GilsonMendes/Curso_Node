1aa827c165b28b80da2af7d963312e2ca346f844
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-05-30 23:21:16 -0300

    Adicionado patas modulos mysql

--- Template_Engine/index.js
+++ Template_Engine/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
+/* const exphbs = require("express-handlebars"); */
+const Handlebars = require("handlebars");
 
 const app = express();
-app.engine("handlebas", exphbs());
-app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
+/* app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars"); */
+/* const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Gilson Mendes" })); */
+
+app.get("/", function (req, res) {
   res.render("home", { layout: false });
 });
+
+app.listen(3000);
---
+++ mysql_integration/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: 'mydatabase'
+});
