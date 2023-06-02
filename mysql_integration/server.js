3b3056e35140c3039bbaf10eeb2b1a93111a55d8
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-06-02 07:56:55 -0300

    Add metodo post e page form

--- mysql_integration/server.js
+++ mysql_integration/server.js
@@ -4,11 +4,27 @@ const mysql = require("mysql2");
 
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
 app.engine("handlebars", exphbs.engine());
+
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.post("/books/inserbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const query = `INSERT INTO CLIENTES(name, email) VALUES ('${nome}', '${email}')`; //revisar
 });
 
 const conn = mysql.createConnection({
@@ -21,7 +37,6 @@ const conn = mysql.createConnection({
 conn.connect(function (err) {
   if (err) {
     console.log(err);
-    
   }
   console.log("Conectou ao MySql");
   app.listen(3000, () => {
